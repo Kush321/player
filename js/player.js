1f37e62e83f0467f26917354f01dc9ade8b0caf0
Kush321 <78313670+Kush321@users.noreply.github.com>
2021-03-24 20:59:02 -0500

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,16 @@
+class Player{
+    constructor(){
+
+    }
+    update(name){
+        var playerIndex="player"+playerCount;
+        database.ref(playerIndex).set({
+            name:name
+        });
+    }
+    updateCount(count){
+        database.ref('/').update({
+            playerCount:count
+        });
+    }
+}
